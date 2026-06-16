df2876dbd01e2be38e2f321853207f01cb21098d
DANIEL ALMEIDA LOVAGLIO <daniel.lovaglio2@senaisp.edu.br>
2026-06-16 12:34:47 -0300

    aaaa

--- src/configs/Database.js
+++ src/configs/Database.js
@@ -52,7 +52,7 @@ export async function initializeDatabase() {
             host: process.env.DB_HOST,
             user: process.env.DB_USER,
             password: process.env.DB_PASSWORD,
-            //database: process.env.DB_DATABASE,
+            database: process.env.DB_DATABASE,
             port: Number(process.env.DB_PORT),
             ssl: {
                 rejectUnauthorized: false
