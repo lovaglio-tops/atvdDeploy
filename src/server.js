41648411348868ba9eb2800db38a0e9896e1e812
DANIEL ALMEIDA LOVAGLIO <daniel.lovaglio2@senaisp.edu.br>
2026-06-16 15:45:23 -0300

    Teste Final

--- src/server.js
+++ src/server.js
@@ -2,11 +2,14 @@ import 'dotenv/config';
 import express from 'express';
 import routes from './routes/routes.js';
 import { initializeDatabase } from './configs/Database.js';
+import cors from 'cors'
+
+configDotenv.config();
 
 
 const app = express();
 
-
+app.use(cors());
 app.use(express.json());
 app.use('/', routes);
 
