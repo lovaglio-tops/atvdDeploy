5584df7e12fbbf115d458e4eaaff9db88ab0562f
DANIEL ALMEIDA LOVAGLIO <daniel.lovaglio2@senaisp.edu.br>
2026-06-16 12:16:29 -0300

    consertando o server

--- src/server.js
+++ src/server.js
@@ -1,25 +1,20 @@
-import express from "express";
-import routes from "./routes/routes.js";
 import 'dotenv/config';
-import cors from 'cors';
+import express from 'express';
+import routes from './routes/routes.js';
 import { initializeDatabase } from './configs/Database.js';
+
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
 
-app.use('/uploads', express.static('uploads'));
+app.use(express.json());
 app.use('/', routes);
 
-initializeDatabase()
-    .then(() => {
-        app.listen(process.env.SERVER_PORT, () => {
-            console.log(
-                `Servidor rodando em http://localhost:${process.env.SERVER_PORT}`
-            );
-        });
-    })
-    .catch((err) => {
-        console.error("Erro ao inicializar o banco:", err);
+
+initializeDatabase().then(() => {
+    app.listen(process.env.SERVER_PORT, () => {
+        console.log(`Servidor rodando na porta ${process.env.SERVER_PORT}`);
     });
+}).catch(err => {
+    console.error("Erro ao inicializar o banco de dados:", err);
+});
